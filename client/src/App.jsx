cdeef8ab7bdb6f680de18d321897dcd816be61f7
rou-nak-cloud <github.rounak.19@gmail.com>
2025-10-10 22:10:18 +0530

    Basic App and Navbar implementation

---
+++ client/src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import {Routes, Route, useLocation } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
+
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
+import Home from './pages/Home'
+import Movies from './pages/Movies'
+import MovieDetails from './pages/MovieDetails'
+import SeatLayout from './pages/SeatLayout'
+import MyBooking from './pages/MyBooking'
+import Favorites from './pages/Favorites'
+
+const App = () => {
+  const isAdminRoute = useLocation().pathname.startsWith('/admin')
+  return (
+    <>
+    <Toaster />
+     {!isAdminRoute && <Navbar />}
+
+     <Routes>
+       <Route path='/' element={<Home />} />
+       <Route path='/movies' element={<Movies />} />
+       <Route path='/movies/:id' element={<MovieDetails />} />
+       <Route path='/movies/:id/:date' element={<SeatLayout />} />
+       <Route path='/my-booking' element={<MyBooking />} />
+       <Route path='/my-booking' element={<Favorites />} />
+     </Routes>
+
+     {!isAdminRoute && <Footer />}
+    </>
+  )
+}
+
+export default App
