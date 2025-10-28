01a138a4f79d88b8908579e6dca6521af0ada18c
rou-nak-cloud <github.rounak.19@gmail.com>
2025-10-28 16:21:15 +0530

    using blur circle component

--- client/src/components/FeaturedSelection.jsx
+++ client/src/components/FeaturedSelection.jsx
@@ -1,6 +1,7 @@
 import { ArrowRight } from 'lucide-react'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import BlurCircle from './BlurCircle'
 
 const FeaturedSelection = () => {
     const navigate = useNavigate()
@@ -8,8 +9,9 @@ const FeaturedSelection = () => {
     <>
      <div className='px-6 md:px-15 lg:px-24 xl:px-44 overflow-hidden'>
         <div className='relative flex items-center justify-between pt-20 pb-10'>
+            <BlurCircle top='0' right='-70px' />
             <p className='text-gray-300 font-medium text-lg'>Now showing</p>
-            <button onClick={()=>navigate('/movies')} className='group flex items-center gap-2 text-sm text-gray-300'>
+            <button onClick={()=>navigate('/movies')} className='group flex items-center gap-2 text-sm text-gray-300 cursor-pointer'>
                 View All
                 <ArrowRight className='group-hover:translate-x-1 transition-all duration-100 w-5 h-5' />
             </button>
