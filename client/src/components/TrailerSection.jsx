fc3b92afec90982ba2efb6fba65648675bee5f81
rou-nak-cloud <github.rounak.19@gmail.com>
2025-10-29 17:02:54 +0530

    updated assetjs file for dummyTrailers and using iFrame instead of reactPLayer

--- client/src/components/TrailerSection.jsx
+++ client/src/components/TrailerSection.jsx
@@ -4,15 +4,20 @@ import { dummyTrailers } from '../assets/assets'
 import BlurCircle from './BlurCircle'
 
 const TrailerSection = () => {
-    const [currentTrailer, setCurrentTrailer] = useState(dummyTrailers[0])
+    const [currentTrailer, setCurrentTrailer] = useState(dummyTrailers[0]);
+    //   console.log(currentTrailer.videoUrl); // 🔍 check URL in console
   return (
     <>
-     <div className='px-6 md:px-16 lg:px-24 xl:px-44 py-20 overflow-hidden'>
+     <div className='px-6 md:px-16 lg:px-24 xl:px-44 py-20 '>
         <p className='text-gray-400 font-medium text-lg max-w-[960px] mx-auto'>Trailers</p>
-        <div className='relative mt-6'>
-            <BlurCircle top='-100px' right='-100px' />
-            <ReactPlayer url={currentTrailer.videoUrl} controls={false} 
-            className='mx-auto max-w-full' width="960px" height="540px" />
+        <div className='relative mt-6 flex justify-center'>
+            <BlurCircle top='100px' right='-150px' />
+            <iframe 
+            src={currentTrailer.videoUrl}
+            title="Trailer" 
+            allowFullScreen
+            allow="picture-in-picture"
+             className='mx-auto max-w-full rounded-xl overflow-hidden shadow-lg' width="960px" height="540px" />
         </div>
      </div>
     </>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
 import FeaturedSelection from '../components/FeaturedSelection'
+import TrailerSection from '../components/TrailerSection'
 
 const home = () => {
   return (
     <>
      <HeroSection />
      <FeaturedSelection />
+     <TrailerSection />
     </>
   )
 }
