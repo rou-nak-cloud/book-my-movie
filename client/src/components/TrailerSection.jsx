9ce7f2006c4cfe0d8fa26f59ba7694b8d2be5f6d
rou-nak-cloud <github.rounak.19@gmail.com>
2025-10-30 14:20:27 +0530

    other trailer section just below

--- client/src/components/TrailerSection.jsx
+++ client/src/components/TrailerSection.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 // import ReactPlayer from 'react-player'
 import { dummyTrailers } from '../assets/assets'
 import BlurCircle from './BlurCircle'
+import {  PlayCircleIcon } from 'lucide-react'
 
 const TrailerSection = () => {
     const [currentTrailer, setCurrentTrailer] = useState(dummyTrailers[0]);
@@ -18,6 +19,16 @@ const TrailerSection = () => {
             allowFullScreen
             allow="picture-in-picture"
              className='mx-auto max-w-full rounded-xl overflow-hidden shadow-lg' width="960px" height="540px" />
+        </div>
+
+        <div className='group grid grid-cols-4 gap-4 md:gap-8 mt-8 max-w-3xl mx-auto'>
+          {dummyTrailers.map((trailer,idx)=>(
+            <div key={idx} onClick={()=>setCurrentTrailer(trailer)} 
+            className='relative group-hover:not-hover:opacity-50 hover:translate-y-2 duration-300 transition-all max-md:h-60 md:max-h-60 cursor-pointer'>
+              <img src={trailer.image} alt="trailer image" className='rounded-lg w-full h-full object-cover brightness-75' />
+              <PlayCircleIcon strokeWidth={1.5} className='absolute top-1/2 left-1/2 w-5 md:w-8 h-5 md:h-12 transform -translate-x-1/2 -translate-y-1/2' />
+            </div>
+          ))}
         </div>
      </div>
     </>
