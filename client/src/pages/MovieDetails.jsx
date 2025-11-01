c1bf6c65d6a30136cb7fa0ca4d62fa1995d5c69d
rou-nak-cloud <github.rounak.19@gmail.com>
2025-11-01 17:04:44 +0530

    add a backdrop path for that movie id for the page

--- client/src/pages/MovieDetails.jsx
+++ client/src/pages/MovieDetails.jsx
@@ -26,25 +26,33 @@ const MovieDetails = () => {
 
 
   return show ?  (
-   <div className='px-6 md:px-16 lg:px-40 pt-30 md:pt-50'>
+    <div className='relative overflow-x-hidden'>
+      {/* Background Image */}
+      <div
+        className='absolute inset-0 -z-10 opacity-40 blur-xs scale-100 bg-center bg-cover transition-all duration-700'
+        style={{ backgroundImage: `url(${show.movie.backdrop_path || show.movie.poster_path})` }}
+      ></div>
+    <div className='px-6 md:px-10 lg:px-30 pt-30 md:pt-70 mb-5 md:mb-10'>
     <div className='flex  flex-col md:flex-row gap-8 max-w-6xl mx-auto'>
       <img src={show.movie.poster_path} alt="showImage" className='max-md:mx-auto rounded-xl h-104 max-w-70 object-cover' />
       <div className='relative flex flex-col gap-3'>
-        <BlurCircle top='-100px' left='-100px' />
+        <BlurCircle top='-100px' left='-200px' />
+        <BlurCircle top='130px' left='-390px' />
         <p className='text-primary'>English</p>
-        <h1 className='text-5xl font-semibold max-w-96 text-balance'>{show.movie.title}</h1>
+        <h1 className='text-5xl font-semibold max-w-116 text-balance'>{show.movie.title}</h1>
         <div className='flex items-center gap-2 text-gray-300'>
           <StarIcon className='w-5 h-5 text-primary fill-primary' />
           {show.movie.vote_average.toFixed(1)} User Rating
         </div>
-        <p className='text-gray-400 mt-2 text-sm leading-tight max-w-xl'>{show.movie.overview}</p>
-        <p>
+        <p className='text-gray-400 mt-2 text-md leading-tight max-w-xl'>{show.movie.overview}</p>
+        <p className='text-gray-300'>
           {timeFormat(show.movie.runtime)} | {show.movie.genres.map(genre=> genre.name).join(", ")} | {show.movie.release_date.split("-")[0]}
         </p>
 
       </div>
     </div>
    </div>
+    </div>
   ) : (
     <div>Loading...</div>
   )
