7a86f381d41482be9b6e827ca539ec23108cedfb
rou-nak-cloud <github.rounak.19@gmail.com>
2025-11-01 16:43:14 +0530

    movie details page with correct _id format

--- client/src/components/MovieCard.jsx
+++ client/src/components/MovieCard.jsx
@@ -9,7 +9,7 @@ const MovieCard = ({movie}) => {
     <>
      <div className='flex flex-col justify-between g-2 p-3 bg-gray-800 rounded-2xl hover:-translate-y-1 transition-all duration-300 w-66'>
         <img 
-        onClick={()=>{navigate('/movies/${movie._id'); scrollTo(0,0)}}
+        onClick={()=>{navigate(`/movies/${movie._id}`); scrollTo(0,0)}}
         src={movie.backdrop_path} alt=""
         className='rounded-lg h-52 w-full object-cover object-right-bottom cursor-pointer' />
 
