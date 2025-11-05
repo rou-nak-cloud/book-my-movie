e50b13b0b899b930cff76172997b641bb2b45f47
rou-nak-cloud <github.rounak.19@gmail.com>
2025-11-05 15:51:42 +0530

    navigation for similar movies

--- client/src/pages/MovieDetails.jsx
+++ client/src/pages/MovieDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { dummyDateTimeData, dummyShowsData } from '../assets/assets'
 import BlurCircle from '../components/BlurCircle'
 import { Heart, PlayCircleIcon, StarIcon } from 'lucide-react'
@@ -8,6 +8,7 @@ import DateSelect from '../components/DateSelect'
 import MovieCard from '../components/MovieCard'
 
 const MovieDetails = () => {
+  const navigate = useNavigate();
   const {id} = useParams()
   console.log(id)
   const [show, setShow] = useState(null)
@@ -31,7 +32,7 @@ const MovieDetails = () => {
     <div className='relative overflow-x-hidden'>
       {/* Background Image */}
       <div
-        className='absolute inset-0 -z-10 opacity-40 blur-xs scale-100 bg-center bg-cover transition-all duration-700'
+        className='absolute h-[40%] inset-0 -z-10 opacity-40 blur-xs scale-100 bg-center bg-cover transition-all duration-700'
         style={{ backgroundImage: `url(${show.movie.backdrop_path || show.movie.poster_path})` }}
       ></div>
     <div className='px-6 md:px-10 lg:px-30 pt-30 md:pt-70 mb-5 md:mb-10'>
@@ -79,12 +80,19 @@ const MovieDetails = () => {
     </div>
 
     <DateSelect dateTime={show.dateTime} id={id} />
-
+    
     <p className='text-lg font-medium mt-20 mb-8'>You may also like</p>
-    <div className='flex flex-wrap max-sm:justify-center gap-8'>
+    <div className='flex flex-wrap md:pl-18 max-sm:justify-center gap-8'>
       {dummyShowsData.slice(0,4).map((movie,idx)=>(
         <MovieCard key={idx} movie={movie} />
       ))}
+    </div>
+    <div className='flex justify-center mt-19'>
+      <button
+      onClick={()=>{navigate('/movies'); scrollTo(0,0)}} 
+      className='px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition-all rounded-md font-medium cursor-pointer'>
+        Show more
+      </button>
     </div>
     
    </div>
