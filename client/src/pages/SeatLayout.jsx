5de519343daae90f9bba4cedcb852e5e81de7c76
rou-nak-cloud <github.rounak.19@gmail.com>
2025-11-05 16:54:29 +0530

    booking date v1

--- client/src/pages/SeatLayout.jsx
+++ client/src/pages/SeatLayout.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { dummyDateTimeData, dummyShowsData } from '../assets/assets'
+import Loading from '../components/Loading'
+import { ClockIcon } from 'lucide-react'
 
 const SeatLayout = () => {
-  return (
-    <div>SeatLayout</div>
+
+  const {id,date} = useParams()
+  const [selectedSeats, setSelectedSeats] = useState([])
+  const [selectedTime, setSelectedTime] = useState(null)
+  const [show, setShow] = useState(null)
+
+  const navigate = useNavigate();
+
+  const getShow = async () => {
+    const show = dummyShowsData.find(show => show._id === id)
+    if(show){
+      setShow({
+        movie:show,
+        dateTime:dummyDateTimeData
+      })
+    }
+    }
+    useEffect(()=>{
+      getShow()
+    },[])
+
+  return show ? (
+    <div className='flex flex-col md:flex-row px-6 md:px-16 lg:px-40 py-30 md:pt-40'>
+      {/* Available timings */}
+      <div className='w-60 bg-primary/20 border border-primary/30 rounded-lg py-10 h-max md:sticky md:top-30'>
+      <p className='text-lg font-semibold px-6'>Available Timings</p>
+        <div className='mt-5 space-y-1'>
+          {show.dateTime[date].map((item)=>(
+          <div className={`flex items-center gap-2 px-6 py-2 w-amx rounded-r-md cursor-pointer transition-all ${selectedTime ?.time === item.time ? "bg-primary text-white" : "hover:bg-primary/30"}`}>
+            <ClockIcon className='w-4 h-4' />
+            <p className='text-sm'>{item.time}</p>
+          </div>
+        ))}
+        </div>
+      </div>
+
+    </div>
+  ) : (
+    <Loading />
   )
 }
 
