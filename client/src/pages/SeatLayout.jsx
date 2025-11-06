8c836108898adcbed735a270a0de652aea586efb
rou-nak-cloud <github.rounak.19@gmail.com>
2025-11-06 15:07:57 +0530

    time format

--- client/src/pages/SeatLayout.jsx
+++ client/src/pages/SeatLayout.jsx
@@ -3,6 +3,7 @@ import { useNavigate, useParams } from 'react-router-dom'
 import { dummyDateTimeData, dummyShowsData } from '../assets/assets'
 import Loading from '../components/Loading'
 import { ClockIcon } from 'lucide-react'
+import isoTimeFormat from '../components/IsoTimeFormat'
 
 const SeatLayout = () => {
 
@@ -35,7 +36,7 @@ const SeatLayout = () => {
           {show.dateTime[date].map((item)=>(
           <div className={`flex items-center gap-2 px-6 py-2 w-amx rounded-r-md cursor-pointer transition-all ${selectedTime ?.time === item.time ? "bg-primary text-white" : "hover:bg-primary/30"}`}>
             <ClockIcon className='w-4 h-4' />
-            <p className='text-sm'>{item.time}</p>
+            <p className='text-sm'>{isoTimeFormat(item.time)}</p>
           </div>
         ))}
         </div>
