905e3de30f9312375d62df2be612d10553e48883
rou-nak-cloud <github.rounak.19@gmail.com>
2025-11-10 16:33:15 +0530

    Admin part initial

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,47 +1,47 @@
-import React from 'react'
-import {Routes, Route, useLocation } from 'react-router-dom'
-import { Toaster } from 'react-hot-toast'
+import React from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
-import Home from './pages/Home'
-import Movies from './pages/Movies'
-import MovieDetails from './pages/MovieDetails'
-import SeatLayout from './pages/SeatLayout'
-import MyBooking from './pages/MyBooking'
-import Favorites from './pages/Favorites'
-import Layout from './admin/Layout'
-import AddShows from './admin/AddShows'
-import ListShows from './admin/ListShows'
-import ListBookings from './admin/ListBookings'
-import Dashboard from './admin/Dashboard'
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import MovieDetails from "./pages/MovieDetails";
+import SeatLayout from "./pages/SeatLayout";
+import MyBooking from "./pages/MyBooking";
+import Favorites from "./pages/Favorites";
+import Layout from "./pages/admin/Layout";
+import AddShows from "./pages/admin/AddShows";
+import ListShows from "./pages/admin/ListShows";
+import ListBookings from "./pages/admin/ListBookings";
+import Dashboard from "./pages/admin/Dashboard";
 
 const App = () => {
-  const isAdminRoute = useLocation().pathname.startsWith('/admin')
+  const isAdminRoute = useLocation().pathname.startsWith("/admin");
   return (
     <>
-    <Toaster />
-     {!isAdminRoute && <Navbar />}
+      <Toaster />
+      {!isAdminRoute && <Navbar />}
 
-     <Routes>
-       <Route path='/' element={<Home />} />
-       <Route path='/movies' element={<Movies />} />
-       <Route path='/movies/:id' element={<MovieDetails />} />
-       <Route path='/movies/:id/:date' element={<SeatLayout />} />
-       <Route path='/my-bookings' element={<MyBooking />} />
-       <Route path='/favorite' element={<Favorites />} />
-       <Route path='/admin/*' element={<Layout />} />
-       <Route index element={<Dashboard />}>
-          <Route path='add-shows' element={<AddShows />} />
-          <Route path='list-shows' element={<ListShows />} />
-          <Route path='list-bookings' element={<ListBookings />} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:id" element={<MovieDetails />} />
+        <Route path="/movies/:id/:date" element={<SeatLayout />} />
+        <Route path="/my-bookings" element={<MyBooking />} />
+        <Route path="/favorite" element={<Favorites />} />
+        <Route path="/admin/*" element={<Layout />} >
+          <Route index element={<Dashboard />} />
+          <Route path="add-shows" element={<AddShows />} />
+          <Route path="list-shows" element={<ListShows />} />
+          <Route path="list-bookings" element={<ListBookings />} />
         </Route>
-     </Routes>
+      </Routes>
 
-     {!isAdminRoute && <Footer />}
+      {!isAdminRoute && <Footer />}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ client/src/components/admin/AdminSidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { assets } from '../../assets/assets'
+
+const AdminSidebar = () => {
+  return (
+   <div>Sidebar</div>
+  )
+}
+
+export default AdminSidebar
