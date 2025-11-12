fcfd618d9743f96e0da4eebb0fa378bbc41fac84
rou-nak-cloud <github.rounak.19@gmail.com>
2025-11-12 15:23:55 +0530

    admin sidebar

--- client/src/components/admin/AdminSidebar.jsx
+++ client/src/components/admin/AdminSidebar.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import { assets } from '../../assets/assets'
+import { LayoutDashboardIcon, PlusSquareIcon, ListIcon, ListCollapseIcon } from 'lucide-react'
 
 const AdminSidebar = () => {
+  const user = {
+    firstName:'Yoo! Admin,',
+    lastName:'BAkshi',
+    imageUrl:assets.profile,
+  }
+
+  const adminNavLinks = [
+    {name: 'Dashboard', path:'/admin',icon: LayoutDashboardIcon },
+    {name: 'Add Shows', path:'/admin/add-shows',icon: PlusSquareIcon },
+    {name: 'List Shows', path:'/admin/list-shows',icon: ListIcon },
+    {name: 'List Bookings', path:'/admin/list-bookings',icon: ListCollapseIcon },
+  ]
   return (
-   <div>Sidebar</div>
+   <div className='h-[calc(100vh-64px)] md:flex flex-col items-center pt-8 max-w-13 md:max-w-60 w-full border-r border-gray-400 text-sm'>
+      <img className='h-9 md:h-14 w-9 md:w-14 rounded-full mx-auto' src={user.imageUrl} alt="Sidebar" />
+      <p className='mt-2 text-base max-md:hidden'>
+        {user.firstName} {user.lastName}
+      </p>
+      <div className='w-full'>
+        {adminNavLinks.map((link,idx)=> (
+          <NavLink key={idx} to={link.path} end className={({ isActive }) => `relative flex items-center max-md:justify-center gap-2 w-full py-2.5 min-md:pl-10 first:mt-6 text-gray-400 ${isActive && 'bg-primary/20 text-primary group'}`}>
+              {({ isActive })=>(
+                <>
+                  <link.icon className='w-5 h-5' />
+                  <p className='max-md:hidden'>{link.name}</p>
+                  <span className={`w-1.5 h-10 rounded-l right-0 absolute ${isActive && 'bg-primary-dull'}`} />
+                </>
+              )}
+          </NavLink>
+        ))}
+      </div>
+   </div>
   )
 }
 
