48b131626fb4fd195134096ae10a36f00404aad9
rou-nak-cloud <github.rounak.19@gmail.com>
2025-11-17 16:43:51 +0530

    clerk with inngest integrations

---
+++ server/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+
+const connectDB = async () => {
+    try {
+        // console.log(process.env.MONGODB_URI);
+        mongoose.connection.on('connected', ()=> console.log(`Database connected`));
+        await mongoose.connect(`${process.env.MONGODB_URI}/MovieSite`)
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+export default connectDB;
--- server/server.js
+++ server/server.js
@@ -1,15 +1,25 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config.js';
+import connectDB from './config/db.js';
+
+import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "./Inngest/inngest.js"
 
 const app = express();
 const port = 7000;
 
+await connectDB();
+
 // Middlewares
 app.use(express.json())
 app.use(cors())
+app.use(clerkMiddleware())
 
 
 // API builders
 app.get('/',(req,res)=> res.send('Server is Live...'))
+app.use('/api/inngest', serve({ client: inngest, functions }))
+
 app.listen(port, ()=> console.log(`Server is Live at port:${port}`));
