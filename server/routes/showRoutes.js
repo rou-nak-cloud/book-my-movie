ba16b00984e11e0a6ac14e95dd2bee5b8ca8bc67
rou-nak-cloud <github.rounak.19@gmail.com>
2025-11-22 14:28:50 +0530

    authentication middleware

--- server/routes/showRoutes.js
+++ server/routes/showRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import { addShow, getNowPlayingMovies } from '../Controllers/showController.js';
+import { protectAdmin } from '../middlewares/auth.js';
 
 const showRouter = express.Router();
 
-showRouter.get('/now-playing', getNowPlayingMovies);
-showRouter.post('/add-show', addShow);
+showRouter.get('/now-playing',protectAdmin, getNowPlayingMovies);
+showRouter.post('/add-show', protectAdmin, addShow);
 
 export default showRouter;
