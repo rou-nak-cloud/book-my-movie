54eec283b31ae976270511fc0cbef60e45c80f36
rou-nak-cloud <github.rounak.19@gmail.com>
2025-11-22 14:41:23 +0530

    routes for those controller files

--- server/routes/showRoutes.js
+++ server/routes/showRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { addShow, getNowPlayingMovies } from '../Controllers/showController.js';
+import { addShow, getNowPlayingMovies, getShow, getShows } from '../Controllers/showController.js';
 import { protectAdmin } from '../middlewares/auth.js';
 
 const showRouter = express.Router();
 
 showRouter.get('/now-playing',protectAdmin, getNowPlayingMovies);
 showRouter.post('/add-show', protectAdmin, addShow);
+showRouter.get('/all',getShows)
+showRouter.get('/:movieId',getShow)
 
 export default showRouter;
