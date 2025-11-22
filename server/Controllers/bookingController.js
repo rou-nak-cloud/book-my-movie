82fd7c965c0fc9fce173de4d84d09f9f9caf163c
rou-nak-cloud <github.rounak.19@gmail.com>
2025-11-22 14:59:07 +0530

    booking controller

---
+++ server/Controllers/bookingController.js
@@ -0,0 +1,73 @@
+import Booking from "../models/Booking.js";
+import Show from "../models/Show.js";
+
+// Function to availability of selected seats for movie
+const checkSeatAvailability = async (showId, selectedSeats) => {
+    try {
+        const showData = await Show.findById(showId);
+        if (!showData) return false;
+
+        const occupiedSeats = showData.occupiedSeats;
+
+        const isAnySeatTaken = selectedSeats.some(seat => occupiedSeats[seat]);
+
+        return !isAnySeatTaken;
+    } catch (error) {
+        console.log(error.message);
+        return false;
+    }
+};
+
+export const createBooking = async (req, res) => {
+    try {
+        const { userId } = req.auth();
+        const { showId, selectedSeats } = req.body;
+        const { origin } = req.headers;
+
+        // check seat availability
+        const isAvailable = checkSeatAvailability(showId, selectedSeats);
+        if (!isAvailable) {
+            return res.json({ success: false, message: 'Selected seats are already booked.' });
+        }
+
+        // get show details
+        const showData = await Show.findById(showId).populate('movie');
+
+        //Create a new booking
+        const booking = await Booking.create({
+            user: userId,
+            show: showId,
+            amount: showData.showPrice * selectedSeats.length,
+            bookedSeats: selectedSeats,
+        });
+
+        selectedSeats.map(seat => {
+            showData.occupiedSeats[seat] = userId;
+        });
+        showData.markModified('occupiedSeats');
+
+        await showData.save();
+
+        // Stripe Gateway Link
+
+
+        res.json({ success: true, message: 'Booking created successfully' });
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+    }
+};
+
+export const getOccupiedSeats = async (req, res) => {
+    try {
+        const { showId } = req.params;
+        const showData = await Show.findById(showId);
+
+        const occupiedSeats = Object.keys(showData.occupiedSeats);
+
+        res.json({ success: true, occupiedSeats });
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+    }
+};
