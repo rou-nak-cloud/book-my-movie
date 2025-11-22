c2c1f45b83f71d1ab82edf0ee12a265ffa496c44
rou-nak-cloud <github.rounak.19@gmail.com>
2025-11-22 14:18:01 +0530

    showController.js

--- server/Controllers/showController.js
+++ server/Controllers/showController.js
@@ -1,6 +1,9 @@
 import axios from "axios"
+import Movie from "../models/Movie.js";
+import Show from "../models/Show.js";
 
 
+// API for nowPlaying
 export const getNowPlayingMovies = async (req,res) => {
     try {
         const { data } = await axios.get('https://api.themoviedb.org/3/movie/now_playing', {
@@ -11,6 +14,73 @@ export const getNowPlayingMovies = async (req,res) => {
         res.json({success:true,movies:movies})
     } catch (error) {
         console.log(error)
+        res.json({success:false,message:error.message})
+    }
+}
+
+export const addShow = async (req,res)=> {
+    try {
+        const {movieId, showsInput, showPrice} = req.body
+        
+        let movie = await Movie.findById(movieId);
+        if(!movie){
+            // Fetch movie details and credits from TMDB API
+            const [movieDetailsResponse,movieCreditResponse] = await Promise.all([
+                axios.get(`https://api.themoviedb.org/3/movie/${movieId}`, {
+                    headers: {
+                        Authorization: `Bearer ${process.env.TMDB_API_KEY}`
+                    }
+                }),
+                axios.get(`https://api.themoviedb.org/3/movie/${movieId}/credits`,{
+                    headers: {
+                        Authorization: `Bearer ${process.env.TMDB_API_KEY}`
+                    }
+                }),
+            ]);
+            const movieApiData = movieDetailsResponse.data;
+            const movieCreditsData = movieCreditResponse.data;
+
+            // inserting in movie model in mongoDb 
+            const movieDetails = {
+                _id: movieId,
+                title:movieApiData.title,
+                overview:movieApiData.overview,
+                poster_path:movieApiData.poster_path,
+                backdrop_path:movieApiData.backdrop_path,
+                genres:movieApiData.genres,
+                casts:movieApiData.cast,
+                release_date:movieApiData.release_date,
+                original_language:movieApiData.original_language,
+                tagline:movieApiData.tagline || '',
+                vote_average:movieApiData.vote_average,
+                runtime:movieApiData.runtime,
+            }
+
+            //  Add movie to database
+            movie = await Movie.create(movieDetails)
+        }
+
+        // Shows
+        const showsToCreate = [];
+        showsInput.forEach(show => {
+            const showDate = show.date;
+            show.time.forEach((time)=>{
+                const dateTimeString = `${showDate}T${time}`;
+                showsToCreate.push({
+                    movie:movieId,
+                    showDateTime: new Date(dateTimeString),
+                    showPrice,
+                    occupiedSeats: {}
+                })
+            })
+        });
+        if(showsToCreate.length > 0){
+            await Show.insertMany(showsToCreate);
+        }
+
+        res.json({success:true,message:'Show added successfully.'})
+    } catch (error) {
+       console.error("TMDB ERROR:", error); 
         res.json({success:false,message:error.message})
     }
 }
--- server/routes/showRoutes.js
+++ server/routes/showRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { getNowPlayingMovies } from '../Controllers/showController.js';
+import { addShow, getNowPlayingMovies } from '../Controllers/showController.js';
 
 const showRouter = express.Router();
 
 showRouter.get('/now-playing', getNowPlayingMovies);
+showRouter.post('/add-show', addShow);
 
 export default showRouter;
