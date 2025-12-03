60ceb0d8486d8c77dee42a34556c43b40e0b9f43
rou-nak-cloud <github.rounak.19@gmail.com>
2025-12-03 16:36:00 +0530

    tweaks regarding stripe booking

--- client/src/pages/MyBooking.jsx
+++ client/src/pages/MyBooking.jsx
@@ -1,70 +1,95 @@
-import React, { useEffect, useState } from 'react'
-import { dummyBookingData } from '../assets/assets'
-import Loading from '../components/Loading'
-import BlurCircle from '../components/BlurCircle'
-import timeFormat from '../components/timeFormat'
-import { dateFormat } from '../components/dateFormat'
-import { useAppContext } from '../context/AppContext'
+import React, { useEffect, useState } from "react";
+import { dummyBookingData } from "../assets/assets";
+import Loading from "../components/Loading";
+import BlurCircle from "../components/BlurCircle";
+import timeFormat from "../components/timeFormat";
+import { dateFormat } from "../components/dateFormat";
+import { useAppContext } from "../context/AppContext";
+import { Link } from "react-router-dom";
 
 const MyBooking = () => {
-  const currency = import.meta.env.VITE_CURRENCY
+  const currency = import.meta.env.VITE_CURRENCY;
 
-     const {axios, getToken,user, image_base_url} = useAppContext()
+  const { axios, getToken, user, image_base_url } = useAppContext();
 
-  const [bookings, setBookings] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
+  const [bookings, setBookings] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-  const getBookings = async ()=> {
+  const getBookings = async () => {
     // setBookings(dummyBookingData)
     // setIsLoading(false)
     try {
-      const {data} = await axios.get(`/api/user/bookings`, {
-        headers: { Authorization: `Bearer ${await getToken()}`}
-      })
-      if(data.success){
-        setBookings(data.bookings)
+      const { data } = await axios.get(`/api/user/bookings`, {
+        headers: { Authorization: `Bearer ${await getToken()}` },
+      });
+      if (data.success) {
+        setBookings(data.bookings);
       }
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-    setIsLoading(false)
-  }
-  useEffect (()=>{
-    if(user){
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    if (user) {
       getBookings();
     }
-  },[user])
+  }, [user]);
 
   return !isLoading ? (
-    <div className='relative px-6 md:px-16 lg:px-40 pt-20 md:pt-40 min-h-[85vh]'>
-      <BlurCircle top='100px' left='100px' />
+    <div className="relative px-6 md:px-16 lg:px-40 pt-20 md:pt-40 min-h-[85vh]">
+      <BlurCircle top="100px" left="100px" />
       <div>
-        <BlurCircle bottom='70px' left='900px' />
+        <BlurCircle bottom="70px" left="900px" />
       </div>
-      <h1 className='text-lg font-semibold mb-4'>My Bookings</h1>
+      <h1 className="text-lg font-semibold mb-4">My Bookings</h1>
 
-      {bookings.map((item,idx)=>(
-        <div key={idx} className='flex flex-col md:flex-row justify-between bg-primary/10  border border-primary/20 rounded-lg mt-4 p-2 max-w-6xl'>
-          <div className='flex flex-col md:flex-row'>
-            <img src={image_base_url + item.show.movie.poster_path} alt="poster path" className='md:max-w-45 aspect-video h-auto object-cover object-bottom rounded' />
-            <div className='flex flex-col p-4'>
-              <p className='text-lg font-semibold'>{item.show.movie.title}</p>
-              <p className='text-gray-300 text-sm'>{timeFormat(item.show.movie.runtime)}</p>
-              <p className='text-gray-300 text-sm mt-auto'>{dateFormat(item.show.showDateTime)}</p>
+      {bookings.map((item, idx) => (
+        <div
+          key={idx}
+          className="flex flex-col md:flex-row justify-between bg-primary/10  border border-primary/20 rounded-lg mt-4 p-2 max-w-6xl"
+        >
+          <div className="flex flex-col md:flex-row">
+            <img
+              src={image_base_url + item.show.movie.poster_path}
+              alt="poster path"
+              className="md:max-w-45 aspect-video h-auto object-cover object-bottom rounded"
+            />
+            <div className="flex flex-col p-4">
+              <p className="text-lg font-semibold">{item.show.movie.title}</p>
+              <p className="text-gray-300 text-sm">
+                {timeFormat(item.show.movie.runtime)}
+              </p>
+              <p className="text-gray-300 text-sm mt-auto">
+                {dateFormat(item.show.showDateTime)}
+              </p>
             </div>
           </div>
 
-          <div className='flex flex-col md:items-end md:text-right justify-between p-4'>
-            <div className='flex items-center gap-4'>
-              <p className='text-2xl font-semibold mb-3 pr-1'>
-                {currency}{item.amount}
+          <div className="flex flex-col md:items-end md:text-right justify-between p-4">
+            <div className="flex items-center gap-4">
+              <p className="text-2xl font-semibold mb-3 pr-1">
+                {currency}
+                {item.amount}
               </p>
-              {!item.isPaid && <button className='bg-primary px-4 py-1.5 mb-3 text-sm rounded-full font-medium cursor-pointer hover:bg-primary-dull transition-all duration-150 ease-in-out'>
-                Pay Now</button>}
+              {!item.isPaid && (
+                <Link
+                  to={item.paymentLink}
+                  className="bg-primary px-4 py-1.5 mb-3 text-sm rounded-full font-medium cursor-pointer hover:bg-primary-dull transition-all duration-150 ease-in-out"
+                >
+                  Pay Now
+                </Link>
+              )}
             </div>
-            <div className='text-sm'>
-              <p><span className='text-gray-400'>Total Tickets:</span>{item.bookedSeats.length}</p>
-              <p><span className='text-gray-400'>Seat Number:</span>{item.bookedSeats.join(", ")}</p>
+            <div className="text-sm">
+              <p>
+                <span className="text-gray-400">Total Tickets:</span>
+                {item.bookedSeats.length}
+              </p>
+              <p>
+                <span className="text-gray-400">Seat Number:</span>
+                {item.bookedSeats.join(", ")}
+              </p>
             </div>
           </div>
         </div>
@@ -72,7 +97,7 @@ const MyBooking = () => {
     </div>
   ) : (
     <Loading />
-  )
-}
+  );
+};
 
-export default MyBooking
+export default MyBooking;
